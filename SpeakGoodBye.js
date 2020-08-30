b0f2d27c200642d40c605400e1ebde420cfcf4e2
HajarOUAAROUCH <69259494+HajarOUAAROUCH@users.noreply.github.com>
2020-08-30 14:42:22 +0100

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,12 +1,12 @@
 // byeSpeaker
 
 (function (window) {
-	var byeSpeaker = {};
-	var speakWord = "Good Bye";
+var byeSpeaker = {};
+var speakWord = "Good Bye";
 
-	var byeSpeaker.speak = function (name) {
-	  console.log(speakWord + " " + name);
-	}
+var byeSpeaker.speak = function (name) {
+	console.log(speakWord + " " + name);
+}
 
 window.byeSpeaker = byeSpeaker;
 
