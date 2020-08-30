e9fee824b5514273ddcaa4655941b7eead9a6d11
HajarOUAAROUCH <69259494+HajarOUAAROUCH@users.noreply.github.com>
2020-08-30 14:43:55 +0100

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,12 +1,12 @@
-// helloSpeaker
-
 (function (window) {
-	var helloSpeaker = {};
-	var speakWord = "Hello";
 
-	helloSpeaker.speak = function (name) {
-	  console.log(speakWord + " " + name);
-	}
+var helloSpeaker = {};
+
+var speakWord = "Hello";
+
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
 
 window.helloSpeaker = helloSpeaker;
 
